60be620ea66cddc04005d3312b5c88dd092778d3
Megagig <megagigsoftwaresolutions.com>
2024-03-12 21:00:54 +0100

    Add the metadata to the layout component for SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'Property rental | Find Your Next Home',
+  description:
+    'Find your dream home with us. We have a wide range of properties to rent.',
+  keywords: 'rent, property, home, house, apartment, flat, real estate, rental',
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
