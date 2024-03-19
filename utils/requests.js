ffe840ce403e9c188e5949e7c06fe0deaa6498f2
Megagig <megagigsoftwaresolutions.com>
2024-03-19 18:57:39 +0100

    Configure the Request file to return an empty array when the domain is not availabe yet

--- utils/requests.js
+++ utils/requests.js
@@ -1,12 +1,19 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
 async function fetchProperties() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+    const res = await fetch(`${apiDomain}/properties`);
     if (!res.ok) {
       throw new Error('failed to fetch properties');
     }
     return res.json();
   } catch (error) {
     console.log(error);
+    return [];
   }
 }
 export { fetchProperties };
